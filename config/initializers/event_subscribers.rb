5b5ec1f2ff8fca1bc895684dbfaee866d08204a1
Niraj Budhathoki <nirajbudhathoki3@gmail.com>
2025-04-14 10:14:33 +0545

    add require

--- config/initializers/event_subscribers.rb
+++ config/initializers/event_subscribers.rb
@@ -1,23 +1,27 @@
 # config/initializers/event_subscribers.rb
+
+require Rails.root.join('app/events/subscribers/order_created_event_handler').to_s
+require Rails.root.join('app/events/subscribers/order_status_changed_event_handler').to_s
+
 Rails.application.config.after_initialize do
-    return if Rails.env.test? || !defined?(::Kafka)
-    
-    begin
-      kafka_config = Rails.application.credentials.kafka
-      
-      subscriber = DataBridgeShared::Clients::EventSubscriber.new(
-        seed_brokers: kafka_config[:brokers],
-        client_id: kafka_config[:client_id],
-        consumer_group: kafka_config[:consumer_group]
-      )
-      
-      # Register event handlers
-      subscriber.subscribe('OrderCreated', Events::Subscribers::OrderCreatedEventHandler.new)
-      subscriber.subscribe('OrderStatusChanged', Events::Subscribers::OrderStatusChangedEventHandler.new)
-      
-      Rails.logger.info "Product Service event subscribers registered successfully"
-    rescue => e
-      Rails.logger.error "Failed to register event subscribers: #{e.message}"
-      Rails.logger.error e.backtrace.join("\n")
-    end
+  return if Rails.env.test? || !defined?(::Kafka)
+
+  begin
+    kafka_config = Rails.application.credentials.kafka
+
+    subscriber = DataBridgeShared::Clients::EventSubscriber.new(
+      seed_brokers: kafka_config[:brokers],
+      client_id: kafka_config[:client_id],
+      consumer_group: kafka_config[:consumer_group]
+    )
+
+    # Register event handlers
+    subscriber.subscribe('OrderCreated', Events::Subscribers::OrderCreatedEventHandler.new)
+    subscriber.subscribe('OrderStatusChanged', Events::Subscribers::OrderStatusChangedEventHandler.new)
+
+    Rails.logger.info "Product Service event subscribers registered successfully"
+  rescue => e
+    Rails.logger.error "Failed to register event subscribers: #{e.message}"
+    Rails.logger.error e.backtrace.join("\n")
   end
+end
